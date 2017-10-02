41c42a3b86bac88eb038bfd03c2de86d3f22f283
Sylwester Guzek <Sylwester.guzek@wp.pl>
2017-10-02 21:55:44 +0200

    Avatar editor employed to pic crop, modal windwow reshaped and adjusted to full screen in mobile

--- src/components/Modlitwa.jsx
+++ src/components/Modlitwa.jsx
@@ -1,15 +1,148 @@
-import React from 'react';
+import React, {Component} from 'react';
+import AvatarEditor from 'react-avatar-editor'
+import Slider from 'material-ui/Slider';
+
+import Crop from 'material-ui/svg-icons/image/crop';
+import FileUpload from 'material-ui/svg-icons/file/file-upload';
+import FlatButton from 'material-ui/FlatButton';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import ActionAndroid from 'material-ui/svg-icons/action/android';
+import FontIcon from 'material-ui/FontIcon';
+
+const styles = {
+  button: {
+    margin: 6,
+  },
+  exampleImageInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+};
 
 
-const Modlitwa = () => (
-  <div className="container">
-    <h2>Modlitwa </h2>
-    <p>Modlitwa Modlitwa Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa </p>
+const min = 0;
+const max = Math.pow(10, 6);
+const power = 12;
+
+function transform(value) {
+  return Math.round((Math.exp(power * value / max) - 1) / (Math.exp(power) - 1) * max);
+}
+
+function reverse(value) {
+  return (1 / power) * Math.log(((Math.exp(power) - 1) * value / max) + 1) * max;
+}
+
+export default class  Modlitwa extends Component {
+
+  state = {
+    slider: Math.pow(10, 4),
+  };
+
+  handleSlider = (event, value) => {
+    this.setState({slider: transform(value)});
+  };
+
+
+	render() {
+		return (
+		  <div className="container" >
+		    <h2>Modlitwa </h2>
+
+		    <div>
+		        <Slider
+		          min={min}
+		          max={max}
+		          step={max / 100}
+		          value={reverse(this.state.slider)}
+		          onChange={this.handleSlider}
+		        />
+		        <p>
+		          <span>{'The value of this slider is: '}</span>
+		          <span>{this.state.slider}</span>
+		        </p>
+		    </div>
+
+		    <p>Modlitwa Modlitwa Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa </p>
+		    <br /> <br /><br /><br /><br />
+
+		    <AvatarEditor
+		        image="./images/avatar.jpg"
+		        width={250}
+		        height={250}
+		        border={50}
+		        color={[255, 255, 255, 0.6]} // RGBA 
+		        scale={1.2}
+		        rotate={0}
+		    />
+
+			<div className="center">
+					<FlatButton
+			      	label="New Image"
+			      	labelPosition="before"
+			      	style={styles.button}
+			      	containerElement="label"
+			      	icon={<FileUpload />}
+			    >
+			    	<input type="file" 
+			    	style={styles.exampleImageInput}
+			    	 />
+			    </FlatButton>
+			    <FlatButton
+			      	label="Crop"
+			      	labelPosition="before"
+			      	primary={true}
+			      	icon={<Crop />}
+			      	style={styles.button}
+			    />
+			</div>
+
+		 <div>
+		    <RaisedButton
+		      label="Upload Image"
+		      labelPosition="before"
+		      style={styles.button}
+		      containerElement="label"
+		      icon={<FileUpload />}
+		    >
+		      <input type="file" style={styles.exampleImageInput} />
+		    </RaisedButton>
+		    <RaisedButton
+		      label="Crop"
+		      labelPosition="before"
+		      style={styles.button}
+		      containerElement="label"
+		      icon={<Crop />}
+		    >
+		    </RaisedButton>
+
+		    <RaisedButton
+		      label="Label before"
+		      labelPosition="before"
+		      primary={true}
+		      icon={<ActionAndroid />}
+		      style={styles.button}
+		    />
+		    <RaisedButton
+		      href="https://github.com/callemall/material-ui"
+		      target="_blank"
+		      label="Github Link"
+		      secondary={true}
+		      style={styles.button}
+		      icon={<FontIcon className="muidocs-icon-custom-github" />}
+		    />
+		  </div>
 
 
 
-    <p>Modlitwa Modlitwa Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa </p>
-  </div>
-);
-
-export default Modlitwa;
+		    <p>Modlitwa Modlitwa Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa  Modlitwa </p>
+		  </div>
+		);
+	}
+}
--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,11 +1,11 @@
-
-
 import React, {Component} from 'react';
 import {Card, CardTitle, CardMedia} from 'material-ui/Card';
+import AvatarEditor from 'react-avatar-editor'
 //icons
 import Email from 'material-ui/svg-icons/maps/local-post-office';
 import Phone from 'material-ui/svg-icons/maps/local-phone';
 import Crop from 'material-ui/svg-icons/image/crop';
+import RotateRight from 'material-ui/svg-icons/image/rotate-right';
 import FileUpload from 'material-ui/svg-icons/file/file-upload';
 import Avatar from 'material-ui/Avatar';
 import List from 'material-ui/List/List';
@@ -20,6 +20,13 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import Slider from 'material-ui/Slider';
+
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
 
 const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
@@ -37,7 +44,22 @@ const styles = {
     width: '100%',
     opacity: 0,
   },
+  appBar: {
+  	// flexWrap: 'wrap',
+   //  width: '100%',
+   //  paddingLeft: '10px',
+   //  paddingRight: '10px',
+    position: "fixed",
+  }
 };
+
+
+// const dialogWindow = {
+//   	width: '100%',
+//   	maxWidth: 'none',
+//   	height: '100%',
+//   	maxHeight: 'none',
+//   };
 
 export default class Profil extends Component {
 	static propTypes: {
@@ -51,18 +73,20 @@ export default class Profil extends Component {
 	    	editWindowOpen: false,
 	    	emailErrorText: '',
 	    	errorMessage: '',
+	    	editFotoMode: false,
+	    	imageScale: 1.2,
+	    	rotate: 0,
 	    	file: this.props.user.foto,
-	    	imagePreviewUrl: baseURL+"/api/avatars/"+this.props.user.foto
+	    	imagePreviewUrl: './images/avatar.jpg',
+	    	imageUrl: baseURL+"/api/avatars/"+this.props.user.foto,
 	    }
   	};
 
-  	componentWillMount() {
-
-  	};
-
-
 	handleOpen = () => {
-  		this.setState({editWindowOpen: true});	
+  		this.setState({
+  			editWindowOpen: true,
+  			imagePreviewUrl:this.state.imageUrl
+  		});	
 	};
 
 	handleClose = () => {
@@ -70,10 +94,13 @@ export default class Profil extends Component {
 	};
 
 	handleSave = () => {
-	   this.setState({editWindowOpen: false});
+	   this.setState({
+	   	editWindowOpen: false,
+	   	imageUrl: this.state.imagePreviewUrl
+		});
 	};
 
-  	onChangeEmail = (event) => {
+  	onEmailChange = (event) => {
   		this.setState({errorMessage:''});
   		this.setState({email:event.target.value})
   		if(this.state.emailErrorText) {
@@ -86,6 +113,7 @@ export default class Profil extends Component {
  	};
 
  	onImageChange = (event) => {
+ 		console.log('on image change triggered');
  		event.preventDefault();
  		let reader = new FileReader();
     	let file = event.target.files[0];
@@ -93,35 +121,76 @@ export default class Profil extends Component {
 	    reader.onloadend = () => {
 	      	this.setState({
 		        file: file,
-		        imagePreviewUrl: reader.result
+		        imagePreviewUrl: reader.result,
+		        editFotoMode: true
 	    	});
  		}
     	reader.readAsDataURL(file)
- 	}
+ 	};
 
+ 	onSliderChange = (event, value) => {
+ 		this.setState({imageScale:value})
+ 	};
+
+ 	onCrop = () => {
+	    if (this.editor) {
+	      // This returns a HTMLCanvasElement, it can be made into a data URL or a blob, 
+	      // drawn on another canvas, or added to the DOM. 
+	      const img = this.editor.getImageScaledToCanvas().toDataURL()
+	      //console.log(img);
+	 	  this.setState({
+	 	  	imagePreviewUrl: img,
+	 	  	editFotoMode: false,
+	 	  });
+
+	      // If you want the image resized to the canvas size (also a HTMLCanvasElement) 
+	      //const canvasScaled = this.editor.getImageScaledToCanvas()
+	    }
+ 	};
+
+ 	setEditorRef = editor => {
+    	if (editor) this.editor = editor
+  	};
+
+  	onRotateRight = (e) => {
+	    e.preventDefault()
+
+	    this.setState({
+	      rotate: this.state.rotate + 90
+	    })
+  	};
+
+  	onCancel = (e) => {
+  		e.preventDefault()
+
+  		this.setState({
+  			imagePreviewUrl: baseURL+"/api/avatars/"+this.props.user.foto,
+  			editFotoMode:false,
+  		})
+  	};
 
 	render() {
 		const { user } = this.props
-	    const actions = [
-	      <FlatButton
-	        label="Cancel"
-	        primary={true}
-	        keyboardFocused={true}
-	        onClick={this.handleClose}
-	      />,
-	      <FlatButton
-	        label="Save"
-	        primary={true}
-	        onClick={this.handleSave}
-	      />,
-	    ];
+	    // const actions = [
+	    //   <FlatButton
+	    //     label="Cancel"
+	    //     primary={true}
+	    //     keyboardFocused={true}
+	    //     onClick={this.handleClose}
+	    //   />,
+	    //   <FlatButton
+	    //     label="Save"
+	    //     primary={true}
+	    //     onClick={this.handleSave}
+	    //   />,
+	    // ];
 
 		return (
 			<div className="container">
 		    	<Card className="center">
 				    <CardMedia overlay={<CardTitle title={user.name}  />} >
 					    <div className="picBox">
-					    	<img className="img-circle" src={`${baseURL}/api/avatars/${user.foto}`} 
+					    	<img className="img-circle" src={this.state.imageUrl} 
 					    	alt="not found" onError={(e)=>{e.target.src='./images/avatar.jpg'}} />
 					    </div>
 				    </CardMedia>
@@ -130,89 +199,157 @@ export default class Profil extends Component {
 	  					<Edit />
 					</FloatingActionButton>
 
-				{/* ----------------------editable list----------------------- */}
+				{/* ----------------------edit profile window----------------------- */}
+
 			        <Dialog
-			          contentClassName="profileEditWindow"			       
-			          title="Edit profile"
-			          actions={actions}
+			          //contentClassName="profileEditWindow"			       
+			          //title="Edit profile"
+			          //actions={actions}
 			          modal={true}
 			          open={this.state.editWindowOpen}
-			          onRequestClose={this.handleClose}			
+			          onRequestClose={this.handleClose}	
+			          autoDetectWindowHeight={true}
+					  autoScrollBodyContent={true}
+					  repositionOnUpdate={true}
+					  //contentStyle={dialogWindow}
+					  bodyClassName="global--modal-body"
+					  contentClassName="global--modal-content"
+				      paperClassName="global--modal-paper"							
 			        >
-			        	<div className="center">
-							<img id="upload-demo" src={this.state.imagePreviewUrl} 
-							  	width="150" height="150" alt="not found"/>	 
-						</div>
-						<input type="hidden" name="foto" id="imageData64"/>
-						
-						<div className="center">
-						    <RaisedButton
-						      label="New Image"
-						      labelPosition="before"
-						      style={styles.button}
-						      containerElement="label"
-						      icon={<FileUpload />}
-						    >
-						      <input type="file" onChange={this.onImageChange} style={styles.exampleImageInput} />
-						    </RaisedButton>
-						    <RaisedButton
-						      label="Crop"
-						      labelPosition="before"
-						      style={styles.button}
-						      containerElement="label"
-						      icon={<Crop />}
-						    >
-						    </RaisedButton>
+			        	<AppBar
+					    title={<span >Edit Profile</span>}
+					    style={styles.appBar}
+					    //onTitleTouchTap={handleTouchTap}
+					    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+					    iconElementRight={<FlatButton label="Save" />}
+					    onLeftIconButtonTouchTap={this.handleClose}
+					    onRightIconButtonTouchTap={this.handleSave}
+					 	/>
+
+					 	<div className="containerModal">
+				        	{!this.state.editFotoMode &&
+							<div className="center">
+		                       	<img id="upload-demo" src={this.state.imagePreviewUrl}
+		                            width="250" height="250" alt="not found"/>
+		                    </div> }
+
+							{this.state.editFotoMode &&	
+					        <div className="center">
+							    <AvatarEditor
+							    	ref={this.setEditorRef}
+							        image={this.state.imagePreviewUrl}
+							        width={250}
+							        height={250}
+							        border={50}
+							        color={[255, 255, 255, 0.6]} // RGBA 
+							        scale={this.state.imageScale}
+							        rotate={this.state.rotate}
+							    />
+							    <Slider value={this.state.imageScale} 
+							    	min={0.5} max={3} 
+							    	onChange={this.onSliderChange} 
+							    	style={{width: 300, margin: 'auto' }}
+							    	sliderStyle={{'marginTop': '5px', 'marginBottom':'5px' }}
+							    	step={3 / 100}
+							    />
+							</div> }
+
+							{!this.state.editFotoMode &&
+								<div className="center">
+								    <RaisedButton
+								      label="New Image"
+								      labelPosition="before"
+								      style={styles.button}
+								      containerElement="label"
+								      icon={<FileUpload />}
+								    >
+								      <input type="file" onChange={this.onImageChange} style={styles.exampleImageInput} />
+								    </RaisedButton> 
+								</div>
+							}					    
+							    
+						    {this.state.editFotoMode &&
+							    <div className="center">
+								    <RaisedButton
+								      label="Crop"
+								      onClick={this.onCrop}
+								      labelPosition="before"
+								      style={styles.button}
+								      containerElement="label"
+								      icon={<Crop />}
+								    >
+								    </RaisedButton>
+								    <RaisedButton							      
+								      onClick={this.onRotateRight}							      
+								      style={styles.button}
+								      containerElement="label"
+								      icon={<RotateRight />}
+								    >
+								    </RaisedButton>
+								    <RaisedButton	
+								      label="Cancel"						      
+								      onClick={this.onCancel}							     
+								      style={styles.button}
+								      containerElement="label"
+
+								    >
+								    </RaisedButton>							    
+								</div>
+							}
+
+
+							<div className="center">
+								<List className="left">
+									<ListItem
+							    	  disabled={true}
+			      					  leftAvatar={ <Avatar icon={<Person />} /> }
+			      					  primaryText={							    
+			      					  	<TextField 
+			      					  	  className="profileTextField"
+									      id="name"
+									      name="name"
+									      //errorText={this.state.emailErrorText}
+									      defaultValue={user.name}
+									      floatingLabelText="Name"
+									      //onChange={this.onEmailChange}
+									    /> }
+			    					> 
+			    					</ListItem>
+							    	<ListItem
+							    	  disabled={true}
+			      					  leftAvatar={ <Avatar icon={<Email />} /> }
+			      					  primaryText={							    
+			      					  	<TextField 
+			      					  	  className="profileTextField"
+									      id="email"
+									      name="email"
+									      errorText={this.state.emailErrorText}
+									      defaultValue={user.email}
+									      floatingLabelText="E-mail"
+									      onChange={this.onEmailChange}
+									    /> }
+			    					> 
+			    					</ListItem>
+							    	<ListItem
+							    	  disabled={true}
+			      					  leftAvatar={ <Avatar icon={<Phone />} /> }
+			      					  primaryText={							    
+			      					  	<TextField 
+			      					  	  className="profileTextField"
+									      id="tel"
+									      name="tel"
+									      //errorText={this.state.emailErrorText}
+									      defaultValue={user.tel}
+									      floatingLabelText="Tel"
+									      //onChange={this.onEmailChange}
+									    /> }
+			    					> 
+			    					</ListItem>
+
+							    </List>
+						    </div>
 						</div>
 
-						<List className="left">
-							<ListItem
-					    	  disabled={true}
-	      					  leftAvatar={ <Avatar icon={<Person />} /> }
-	      					  primaryText={							    
-	      					  	<TextField 
-	      					  	  className="profileTextField"
-							      id="name"
-							      name="name"
-							      //errorText={this.state.emailErrorText}
-							      defaultValue={user.name}
-							      floatingLabelText="Name"
-							      //onChange={this.onChangeEmail}
-							    /> }
-	    					> 
-	    					</ListItem>
-					    	<ListItem
-					    	  disabled={true}
-	      					  leftAvatar={ <Avatar icon={<Email />} /> }
-	      					  primaryText={							    
-	      					  	<TextField 
-	      					  	  className="profileTextField"
-							      id="email"
-							      name="email"
-							      errorText={this.state.emailErrorText}
-							      defaultValue={user.email}
-							      floatingLabelText="E-mail"
-							      onChange={this.onChangeEmail}
-							    /> }
-	    					> 
-	    					</ListItem>
-					    	<ListItem
-					    	  disabled={true}
-	      					  leftAvatar={ <Avatar icon={<Phone />} /> }
-	      					  primaryText={							    
-	      					  	<TextField 
-	      					  	  className="profileTextField"
-							      id="tel"
-							      name="tel"
-							      //errorText={this.state.emailErrorText}
-							      defaultValue={user.tel}
-							      floatingLabelText="Tel"
-							      //onChange={this.onChangeEmail}
-							    /> }
-	    					> 
-	    					</ListItem>
-
-					    </List>
 			        </Dialog>			
 
 			        {/* --------------static list read-only------------------*/}
