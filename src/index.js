51647609e2918331986508e99424e30295745fa2
Sylwester Guzek <Sylwester.guzek@wp.pl>
2017-10-20 12:40:18 +0200

    standard react serviceworker implemented, spinner loading on index page added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 import './css/index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+registerServiceWorker();
